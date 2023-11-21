2f4e909848106ef5b4f7f8641902e1b251927a46
Reis567 <matheusdosreis9@gmail.com>
2023-11-21 18:55:07 -0300

    decimal places and max digit of rate

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Avaliacao(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField(null=True,blank=True)
-    nota = models.DecimalField()
+    nota = models.DecimalField(max_digits=3, decimal_places=2)
     data = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.user.username
