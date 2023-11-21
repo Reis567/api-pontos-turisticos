8f04a70a0c59cedfd12c3de8210b505a5169e373
Reis567 <matheusdosreis9@gmail.com>
2023-11-21 17:34:11 -0300

    model of PontoTuristico

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    status = models.BooleanField()
+
+    def __str__(self):
+        return self.nome
+    
