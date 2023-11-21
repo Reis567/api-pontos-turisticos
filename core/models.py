38d96244af3b99970425df00d30a778931e052d2
Reis567 <matheusdosreis9@gmail.com>
2023-11-21 17:43:10 -0300

    aproved default = false

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
-    status = models.BooleanField()
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nome
