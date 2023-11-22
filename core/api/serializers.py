7fd867b5e1e3f6673c9dd092fa3493bd2ceb49ef
Reis567 <matheusdosreis9@gmail.com>
2023-11-22 08:07:55 -0300

    viewset from pontoturistico

---
+++ core/api/serializers.py
@@ -0,0 +1,7 @@
+from core.models import PontoTuristico
+from rest_framework.serializers import ModelSerializer
+
+class PontoTuristicoSerializer(ModelSerializer):
+    class Meta:
+        model = PontoTuristico
+        fields = ('id','nome','descricao')
