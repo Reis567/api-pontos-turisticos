a3e54286c9d13827e53cdaaad204e59ba767a080
Reis567 <matheusdosreis9@gmail.com>
2023-11-23 08:18:31 -0300

    rate app serializer

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -8,6 +8,7 @@ class Avaliacao(models.Model):
     comentario = models.TextField(null=True,blank=True)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
     data = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.user.username
     
