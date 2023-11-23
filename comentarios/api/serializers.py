3e95289573eca03e1222a4733277312223191318
Reis567 <matheusdosreis9@gmail.com>
2023-11-23 08:12:46 -0300

    serializer name fix

--- comentarios/api/serializers.py
+++ comentarios/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from ..models import Comentario
 
 
-class AtracaoSerializer(ModelSerializer):
+class ComentarioSerializer(ModelSerializer):
     class Meta:
         model = Comentario
         fields = ('usuario','comentario','data','aprovado')
