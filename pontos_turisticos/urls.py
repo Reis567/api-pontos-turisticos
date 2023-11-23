0c8a6659bc79345ab5a748a0e52755c7ac139759
Reis567 <matheusdosreis9@gmail.com>
2023-11-23 19:28:44 -0300

    fix , static to media url

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -37,4 +37,4 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
