8b2bb35f04609d44ee145e21e6dc24eb662fa220
Reis567 <matheusdosreis9@gmail.com>
2023-11-24 07:39:06 -0300

    search in ataction filter

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,7 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Atracao
 from .serializers import AtracaoSerializer
 
-class AtracaoViewSet(ModelViewSet):
+class AtracaoViewSet(viewsets.ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('nome', 'descricao')
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'core',
     'atracoes',
     'comentarios',
     'avaliacoes',
     'endereco',
+    'django_filters',
 
 ]
 
