9cd7657b522a3f31289ad73919d10de5e2821478
Reis567 <matheusdosreis9@gmail.com>
2023-11-24 06:48:19 -0300

    filter on core viewset

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -6,5 +6,18 @@ class PontoTuristicoViewset(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(status=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects
+
+        if id:
+            queryset.filter(pk=id)
+        if nome:
+            queryset.filter(nome__iexact=nome)
+        if descricao:
+            queryset.filter(descricao__iexact=descricao)
+
+        
+        return queryset
     
