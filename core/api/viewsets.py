8b611aa099c39ca87c496679592d88a0c784a0b4
Reis567 <matheusdosreis9@gmail.com>
2023-11-27 07:22:12 -0300

    authentication method

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,10 +2,14 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.authentication import TokenAuthentication
 
 class PontoTuristicoViewset(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     filter_backends = (SearchFilter,)
+    permission_classes = (DjangoModelPermissions,)
+    authentication_classes  = (TokenAuthentication,)
     search_fields = ('nome','descricao','endereco__linha1')
 
     def get_queryset(self):
