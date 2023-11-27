6f4b11a013100d62e21d048fa84cd53d8c193b78
Reis567 <matheusdosreis9@gmail.com>
2023-11-27 07:43:26 -0300

    atracoes object

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,9 @@
 from ..models import PontoTuristico
 from rest_framework.serializers import ModelSerializer
+from atracoes.api.serializers import AtracaoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
     class Meta:
         model = PontoTuristico
         fields = ('id','nome','descricao','status', 'foto', 'atracoes','comentarios','avaliacoes','endereco')
