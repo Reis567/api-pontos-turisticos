51ddc6d3009f8e017fdb3fd4800bd277ff07c978
Reis567 <matheusdosreis9@gmail.com>
2023-11-27 07:45:13 -0300

    rate serializer object

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -2,11 +2,12 @@ from ..models import PontoTuristico
 from rest_framework.serializers import ModelSerializer
 from atracoes.api.serializers import AtracaoSerializer
 from comentarios.api.serializers import ComentarioSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     comentarios = ComentarioSerializer(many=True)
-    
+    avaliacoes = AvaliacaoSerializer(many=True)
     class Meta:
         model = PontoTuristico
         fields = ('id','nome','descricao','status', 'foto', 'atracoes','comentarios','avaliacoes','endereco')
