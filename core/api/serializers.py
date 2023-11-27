a8983a135acca0c8936d2068f762dd6059d6b6d8
Reis567 <matheusdosreis9@gmail.com>
2023-11-27 07:39:27 -0300

    another fields in serializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,4 +4,4 @@ from rest_framework.serializers import ModelSerializer
 class PontoTuristicoSerializer(ModelSerializer):
     class Meta:
         model = PontoTuristico
-        fields = ('id','nome','descricao','status', 'foto')
+        fields = ('id','nome','descricao','status', 'foto', 'atracoes','comentarios','avaliacoes','endereco')
