4a0add3b8719be58c3a14f0edead60e267a73b36
Reis567 <matheusdosreis9@gmail.com>
2023-11-27 07:47:48 -0300

    adress serializer object

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -3,11 +3,13 @@ from rest_framework.serializers import ModelSerializer
 from atracoes.api.serializers import AtracaoSerializer
 from comentarios.api.serializers import ComentarioSerializer
 from avaliacoes.api.serializers import AvaliacaoSerializer
+from endereco.api.serializers import EnderecoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
     comentarios = ComentarioSerializer(many=True)
     avaliacoes = AvaliacaoSerializer(many=True)
+    endereco = EnderecoSerializer()
     class Meta:
         model = PontoTuristico
         fields = ('id','nome','descricao','status', 'foto', 'atracoes','comentarios','avaliacoes','endereco')
